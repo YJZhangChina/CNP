abf7d4c4802c0fae04fe2d2d225b11bd11e630f5
YJZhangChina <zhangyingjie.china@gmail.com>
2019-05-12 20:56:29 +0800

    Learning linux system.

---
+++ c/thread/single.c
@@ -0,0 +1,24 @@
+//
+// Created by zhangyingjie on 19-4-8.
+//
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define NUM 5
+
+void virtual_main() {
+    void print_msg(char *);
+    print_msg("hello\n");
+    print_msg("world\n");
+}
+
+void print_msg(char *m) {
+    int i;
+    for(i = 0; i < NUM; ++i) {
+        printf("%s", m);
+        fflush(stdout);
+        sleep(1);
+    }
+}
